e9af714f14bf07467e0b262a263eb8752090793d
Gina Yuan <gina.yuan@gmail.com>
2019-12-22 23:11:15 -0800

    Factor out numpy blackscholes code

--- python/gpu-benchmarks/workloads/blackscholes_numpy.py
+++ python/gpu-benchmarks/workloads/blackscholes_numpy.py
@@ -68,6 +68,87 @@ def get_tmp_arrays(mode, size):
     return tmp, vol_sqrt, rsig, d1, d2, call, put
 
 
+def numpy_bs(price, strike, t, rate, vol, tmp, vol_sqrt, rsig, d1, d2, call, put, composer):
+    if composer:
+        import sa.annotated.numpy as np
+    else:
+        import numpy as np
+
+    c05 = 3.0
+    c10 = 1.5
+    invsqrt2 = 1.0 / math.sqrt(2.0)
+
+    np.multiply(vol, vol, out=rsig)
+    np.multiply(rsig, c05, out=rsig)
+    np.add(rsig, rate, out=rsig)
+
+    np.sqrt(t, out=vol_sqrt)
+    np.multiply(vol_sqrt, vol, out=vol_sqrt)
+
+    np.multiply(rsig, t, out=tmp)
+    np.divide(price, strike, out=d1)
+    np.log2(d1, out=d1)
+    np.add(d1, tmp, out=d1)
+
+    np.divide(d1, vol_sqrt, out=d1)
+    np.subtract(d1, vol_sqrt, out=d2)
+
+    # d1 = c05 + c05 * erf(d1 * invsqrt2)
+    np.multiply(d1, invsqrt2, out=d1)
+
+    if composer:
+        np.erf(d1, out=d1)
+    else:
+        ss.erf(d1, out=d1)
+
+    np.multiply(d1, c05, out=d1)
+    np.add(d1, c05, out=d1)
+
+    # d2 = c05 + c05 * erf(d2 * invsqrt2)
+    np.multiply(d2, invsqrt2, out=d2)
+
+    if composer:
+        np.erf(d2, out=d2)
+    else:
+        ss.erf(d2, out=d2)
+
+    np.multiply(d2, c05, out=d2)
+    np.add(d2, c05, out=d2)
+
+    # Reuse existing buffers
+    e_rt = vol_sqrt
+    tmp2 = rsig
+
+    # e_rt = exp(-rate * t)
+    np.multiply(rate, -1.0, out=e_rt)
+    np.multiply(e_rt, t, out=e_rt)
+    np.exp(e_rt, out=e_rt)
+
+    # call = price * d1 - e_rt * strike * d2
+    #
+    # tmp = price * d1
+    # tmp2 = e_rt * strike * d2
+    # call = tmp - tmp2
+    np.multiply(price, d1, out=tmp)
+    np.multiply(e_rt, strike, out=tmp2)
+    np.multiply(tmp2, d2, out=tmp2)
+    np.subtract(tmp, tmp2, out=call)
+
+    # put = e_rt * strike * (c10 - d2) - price * (c10 - d1)
+    # tmp = e_rt * strike
+    # tmp2 = (c10 - d2)
+    # put = tmp - tmp2
+    # tmp = c10 - d1
+    # tmp = price * tmp
+    # put = put - tmp
+    np.multiply(e_rt, strike, out=tmp)
+    np.subtract(c10, d2, out=tmp2)
+    np.multiply(tmp, tmp2, out=put)
+    np.subtract(c10, d1, out=tmp)
+    np.multiply(price, tmp, out=tmp)
+    np.subtract(put, tmp, out=put)
+
+
 def run_composer(
     mode,
     price, strike, t, rate, vol, tmp, vol_sqrt, rsig, d1, d2, call, put,
@@ -85,74 +166,10 @@ def run_composer(
     else:
         raise Exception
 
-    c05 = 3.0
-    c10 = 1.5
-    invsqrt2 = 1.0 / math.sqrt(2.0)
-
-    np.multiply(vol, vol, out=rsig)
-    np.multiply(rsig, c05, out=rsig)
-    np.add(rsig, rate, out=rsig)
-
-    np.sqrt(t, out=vol_sqrt)
-    np.multiply(vol_sqrt, vol, out=vol_sqrt)
-
-    np.multiply(rsig, t, out=tmp)
-    np.divide(price, strike, out=d1)
-    np.log2(d1, out=d1)
-    np.add(d1, tmp, out=d1)
-
-    np.divide(d1, vol_sqrt, out=d1)
-    np.subtract(d1, vol_sqrt, out=d2)
-
-    # d1 = c05 + c05 * erf(d1 * invsqrt2)
-    np.multiply(d1, invsqrt2, out=d1)
-    np.erf(d1, out=d1)
-    np.multiply(d1, c05, out=d1)
-    np.add(d1, c05, out=d1)
-
-    # d2 = c05 + c05 * erf(d2 * invsqrt2)
-    np.multiply(d2, invsqrt2, out=d2)
-    np.erf(d2, out=d2)
-    np.multiply(d2, c05, out=d2)
-    np.add(d2, c05, out=d2)
-
-    # Reuse existing buffers
-    e_rt = vol_sqrt
-    tmp2 = rsig
-
-    # e_rt = exp(-rate * t)
-    np.multiply(rate, -1.0, out=e_rt)
-    np.multiply(e_rt, t, out=e_rt)
-    np.exp(e_rt, out=e_rt)
-
-    # call = price * d1 - e_rt * strike * d2
-    #
-    # tmp = price * d1
-    # tmp2 = e_rt * strike * d2
-    # call = tmp - tmp2
-    np.multiply(price, d1, out=tmp)
-    np.multiply(e_rt, strike, out=tmp2)
-    np.multiply(tmp2, d2, out=tmp2)
-    np.subtract(tmp, tmp2, out=call)
-
-    # put = e_rt * strike * (c10 - d2) - price * (c10 - d1)
-    # tmp = e_rt * strike
-    # tmp2 = (c10 - d2)
-    # put = tmp - tmp2
-    # tmp = c10 - d1
-    # tmp = price * tmp
-    # put = put - tmp
-    np.multiply(e_rt, strike, out=tmp)
-    np.subtract(c10, d2, out=tmp2)
-    np.multiply(tmp, tmp2, out=put)
-    np.subtract(c10, d1, out=tmp)
-    np.multiply(price, tmp, out=tmp)
-    np.subtract(put, tmp, out=put)
-
+    numpy_bs(price, strike, t, rate, vol, tmp, vol_sqrt, rsig, d1, d2, call, put, composer=True)
     np.evaluate(workers=threads, batch_size=batch_size, force_cpu=force_cpu)
     if not force_cpu:
         torch.cuda.synchronize()
-
     return call.value, put.value
 
 
@@ -160,66 +177,7 @@ def run_naive(price, strike, t, rate, vol, tmp, vol_sqrt, rsig, d1, d2, call, pu
     c05 = 3.0
     c10 = 1.5
     invsqrt2 = 1.0 / math.sqrt(2.0)
-
-    np.multiply(vol, vol, out=rsig)
-    np.multiply(rsig, c05, out=rsig)
-    np.add(rsig, rate, out=rsig)
-
-    np.sqrt(t, out=vol_sqrt)
-    np.multiply(vol_sqrt, vol, out=vol_sqrt)
-
-    np.multiply(rsig, t, out=tmp)
-    np.divide(price, strike, out=d1)
-    np.log2(d1, out=d1)
-    np.add(d1, tmp, out=d1)
-
-    np.divide(d1, vol_sqrt, out=d1)
-    np.subtract(d1, vol_sqrt, out=d2)
-
-    # d1 = c05 + c05 * erf(d1 * invsqrt2)
-    np.multiply(d1, invsqrt2, out=d1)
-    ss.erf(d1, out=d1)
-    np.multiply(d1, c05, out=d1)
-    np.add(d1, c05, out=d1)
-
-    # d2 = c05 + c05 * erf(d2 * invsqrt2)
-    np.multiply(d2, invsqrt2, out=d2)
-    ss.erf(d2, out=d2)
-    np.multiply(d2, c05, out=d2)
-    np.add(d2, c05, out=d2)
-
-    # Reuse existing buffers
-    e_rt = vol_sqrt
-    tmp2 = rsig
-
-    # e_rt = exp(-rate * t)
-    np.multiply(rate, -1.0, out=e_rt)
-    np.multiply(e_rt, t, out=e_rt)
-    np.exp(e_rt, out=e_rt)
-
-    # call = price * d1 - e_rt * strike * d2
-    #
-    # tmp = price * d1
-    # tmp2 = e_rt * strike * d2
-    # call = tmp - tmp2
-    np.multiply(price, d1, out=tmp)
-    np.multiply(e_rt, strike, out=tmp2)
-    np.multiply(tmp2, d2, out=tmp2)
-    np.subtract(tmp, tmp2, out=call)
-
-    # put = e_rt * strike * (c10 - d2) - price * (c10 - d1)
-    # tmp = e_rt * strike
-    # tmp2 = (c10 - d2)
-    # put = tmp - tmp2
-    # tmp = c10 - d1
-    # tmp = price * tmp
-    # put = put - tmp
-    np.multiply(e_rt, strike, out=tmp)
-    np.subtract(c10, d2, out=tmp2)
-    np.multiply(tmp, tmp2, out=put)
-    np.subtract(c10, d1, out=tmp)
-    np.multiply(price, tmp, out=tmp)
-    np.subtract(put, tmp, out=put)
+    numpy_bs(price, strike, t, rate, vol, tmp, vol_sqrt, rsig, d1, d2, call, put, composer=False)
     return call, put
 
 
