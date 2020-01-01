9c05156cfac8130086c7e41cab87db37b58a0dd5
Gina Yuan <gina.yuan@gmail.com>
2019-12-31 18:24:41 -0800

    Normal gradient descent classification cuda and naive

--- python/gpu-benchmarks/workloads/sgd_classifier.py
+++ python/gpu-benchmarks/workloads/sgd_classifier.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 import sys
 import time
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.linear_model import SGDClassifier
+from cuml.solvers import SGD as cumlSGD
+import cudf
 
 sys.path.append("../../lib")
 sys.path.append("../../pycomposer")
@@ -9,25 +17,66 @@ sys.path.append(".")
 from sa.annotation import Backend
 from mode import Mode
 
-DEFAULT_SIZE = 1 << 26
+DEFAULT_SIZE = 1 << 12
 DEFAULT_CPU = 1 << 16
 DEFAULT_GPU = 1 << 26
 
+DEFAULT_INFORMATIVE = 32
+DEFAULT_FEATURES = 256
+DEFAULT_PREDICTIONS = 1 << 11
 
-def get_data(mode, size):
-    raise Exception
+
+def _gen_data_cuda(X0, y0, columns, predictions):
+    X = pd.DataFrame(X0[:-predictions], columns=columns)
+    pred_data = pd.DataFrame(X0[-predictions:], columns=columns)
+    X = cudf.from_pandas(X)
+    pred_data = cudf.from_pandas(pred_data)
+
+    y = cudf.Series(y0[:-predictions], dtype='float64')
+    pred_results = pd.Series(y0[-predictions:], dtype='float64')
+    return X, y, pred_data, pred_results
+
+
+def _gen_data(X0, y0, columns, predictions):
+    X = pd.DataFrame(X0[:-predictions], columns=columns)
+    y = pd.Series(y0[:-predictions])
+    pred_data = pd.DataFrame(X0[-predictions:], columns=columns)
+    pred_results = pd.Series(y0[-predictions:])
+    return X, y, pred_data, pred_results
+
+
+def gen_data(
+    mode,
+    size,
+    informative=DEFAULT_INFORMATIVE,
+    features=DEFAULT_FEATURES,
+    predictions=DEFAULT_PREDICTIONS,
+):
+    X0, y0 = datasets.make_classification(n_samples=size + predictions, n_features=features,
+                                          n_informative=informative, n_classes=2, random_state=0)
+    columns = [str(i) for i in range(features)]
+    if mode == Mode.CUDA:
+        return _gen_data_cuda(X0, y0, columns, predictions)
+    else:
+        return _gen_data(X0, y0, columns, predictions)
 
 
 def run_composer(mode, inputs, batch_size, threads):
     raise Exception
 
 
-def run_naive(inputs):
-    raise Exception
+def run_naive(X, y, pred_data):
+    sgd = SGDClassifier(eta0=0.005, max_iter=2000, fit_intercept=True, tol=1e-3)
+    sgd.fit(X, y)
+    pred = sgd.predict(pred_data)
+    return sgd.intercept_, pred
 
 
-def run_cuda(inputs):
-    raise Exception
+def run_cuda(X, y, pred_data):
+    cu_sgd = cumlSGD(eta0=0.005, epochs=2000, fit_intercept=True, tol=1e-3)
+    cu_sgd.fit(X, y)
+    cu_pred = cu_sgd.predictClass(pred_data).to_array()
+    return cu_sgd.intercept_, cu_pred
 
 
 def run(mode, size=None, cpu=None, gpu=None, threads=None, data_mode='file'):
@@ -51,24 +100,27 @@ def run(mode, size=None, cpu=None, gpu=None, threads=None, data_mode='file'):
 
     # Get inputs
     start = time.time()
-    inputs = get_data(mode, size)
+    X, y, pred_data, pred_results = gen_data(mode, size)
+    inputs = (X, y, pred_data)
     init_time = time.time() - start
-    print('Initialization:', init_time)
+    sys.stdout.write('Initialization: {}\n'.format(init_time))
 
     # Run program
     start = time.time()
     if mode.is_composer():
-        result = run_composer(mode, *inputs, batch_size, threads)
+        intercept, pred = run_composer(mode, *inputs, batch_size, threads)
     elif mode == Mode.NAIVE:
-        result = run_naive(*inputs)
+        intercept, pred = run_naive(*inputs)
     elif mode == Mode.CUDA:
-        result = run_cuda(*inputs)
+        intercept, pred = run_cuda(*inputs)
     else:
         raise ValueError
     runtime = time.time() - start
 
-    print('Runtime:', runtime)
+    sys.stdout.write('Runtime: {}\n'.format(runtime))
     sys.stdout.write('Total: {}\n'.format(init_time + runtime))
     sys.stdout.flush()
-    print(result)
+    print(intercept)
+    print(pred)
     return init_time, runtime
+
