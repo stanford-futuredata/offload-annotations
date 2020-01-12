b9b9bccaebd195bd3cc756362b73a8eed1381015
Deepak Narayanan <deepakn94@gmail.com>
2020-01-11 23:40:18 -0800

    Taxi rider workload

---
+++ python/gpu-benchmarks/workloads/taxi_rider_stats.py
@@ -0,0 +1,33 @@
+import sys
+import time
+
+from dask.distributed import Client
+from dask_cuda import LocalCUDACluster
+import dask_cudf
+
+def run_naive():
+    cluster = LocalCUDACluster(ip='0.0.0.0',n_workers=1, device_memory_limit='10000 MiB')
+    client = Client(cluster)
+
+    start = time.time()
+    df = dask_cudf.read_csv('rows.csv', parse_dates=['tpep_pickup_datetime'])
+    df.persist()
+    init_time = time.time() - start
+    sys.stdout.write('Initialization: {}\n'.format(init_time))
+
+    start = time.time()
+    # Average trip distance, grouped by passenger count.
+    df.groupby('passenger_count').trip_distance.mean().compute()
+    # Average tip fraction, grouped by hour of trip.
+    df2 = df[['tpep_pickup_datetime', 'trip_distance', 'tip_amount', 'fare_amount']]
+    df2 = df2.query('tip_amount > 0 and fare_amount > 0')
+    df2['hour'] = df2.tpep_pickup_datetime.dt.hour
+    df2['tip_fraction'] = df2.tip_amount / df2.fare_amount
+    df2.groupby('hour').tip_fraction.mean().compute().to_pandas()
+    runtime = time.time() - start
+    sys.stdout.write('Runtime: {}\n'.format(runtime))
+    sys.stdout.write('Total: {}\n'.format(init_time + runtime))
+
+
+if __name__ == '__main__':
+    run_naive()
