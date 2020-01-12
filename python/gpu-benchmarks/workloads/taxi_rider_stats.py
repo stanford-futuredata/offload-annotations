9da95c0b08df8f910c8d33aeda935e4b94911d85
Deepak Narayanan <deepakn94@gmail.com>
2020-01-12 11:07:42 -0800

    Add CPU-only baseline to compare to CUDA baseline

--- python/gpu-benchmarks/workloads/taxi_rider_stats.py
+++ python/gpu-benchmarks/workloads/taxi_rider_stats.py
@@ -4,9 +4,29 @@ import time
 from dask.distributed import Client
 from dask_cuda import LocalCUDACluster
 import dask_cudf
+import pandas as pd
 
 def run_naive():
-    cluster = LocalCUDACluster(ip='0.0.0.0',n_workers=1, device_memory_limit='10000 MiB')
+    start = time.time()
+    df = pd.read_csv('nyc_taxi.csv', parse_dates=['tpep_pickup_datetime'])
+    init_time = time.time() - start
+    sys.stdout.write('Initialization: {}\n'.format(init_time))
+
+    start = time.time()
+    # Average trip distance, grouped by passenger count.
+    df.groupby('passenger_count').trip_distance.mean()
+    # Average tip fraction, grouped by hour of trip.
+    df2 = df[['tpep_pickup_datetime', 'trip_distance', 'tip_amount', 'fare_amount']]
+    df2 = df2.query('tip_amount > 0 and fare_amount > 0')
+    df2['hour'] = df2.tpep_pickup_datetime.dt.hour
+    df2['tip_fraction'] = df2.tip_amount / df2.fare_amount
+    df2.groupby('hour').tip_fraction.mean()
+    runtime = time.time() - start
+    sys.stdout.write('Runtime: {}\n'.format(runtime))
+    sys.stdout.write('Total: {}\n'.format(init_time + runtime))
+
+def run_cuda():
+    cluster = LocalCUDACluster(ip='0.0.0.0',n_workers=4, device_memory_limit='10000 MiB')
     client = Client(cluster)
 
     start = time.time()
@@ -30,4 +50,5 @@ def run_naive():
 
 
 if __name__ == '__main__':
+    run_cuda()
     run_naive()
