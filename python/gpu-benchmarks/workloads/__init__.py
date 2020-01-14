6e5291bd2514842c6aeaf1853bd2e2d8a5da709a
Gina Yuan <gina.yuan@gmail.com>
2020-01-13 17:22:14 -0800

    Comment out unused workloads

--- python/gpu-benchmarks/workloads/__init__.py
+++ python/gpu-benchmarks/workloads/__init__.py
@@ -3,13 +3,13 @@ from . import blackscholes_numpy
 from . import blackscholes_torch
 from . import crime_index
 from . import sgd_classifier
-from . import tsvd
+# from . import tsvd
 from . import pca
 from . import dbscan
-from . import image_processing
+# from . import image_processing
 from . import blackscholes_cupy
 from . import haversine
-from . import kidneys
-from . import clusterdim
-from . import feature_engineering
-from . import taxi_rider_stats
+# from . import kidneys
+# from . import clusterdim
+# from . import feature_engineering
+# from . import taxi_rider_stats
