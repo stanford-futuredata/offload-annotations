27be8ce83f2f910555ffe81a5558e43d9050f6fe
Gina Yuan <gina.yuan@gmail.com>
2020-01-16 12:53:33 -0800

    Match cuDF to numpy conversion in tsvd workload

--- python/pycomposer/sa/annotated/cupy/annotated.py
+++ python/pycomposer/sa/annotated/cupy/annotated.py
@@ -77,7 +77,7 @@ class NdArraySplit(SplitType):
                 return cp.asnumpy(value)
             else:
                 # Also convert back from cuDF objects
-                return value.to_pandas().to_numpy()
+                return np.asarray(value.as_gpu_matrix())
         else:
             raise Exception('cannot transfer from {} to {}'.format(current_backend, backend))
 
